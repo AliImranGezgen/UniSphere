1872c41ab68d63cca0f9ab229c0bfe6f4162e555
Ömer Pürlü <omrprl.00@gmail.com>
2026-03-22 22:37:44 +0300

    fix: App.tsx çakışması çözüldü, routing eklendi

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
-import React from "react";
 import EventForm from "./components/EventForm";
 
 // Ana uygulama bileşeni
