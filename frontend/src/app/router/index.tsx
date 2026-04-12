8c48eb6b58846ca5cb4971d8ddb5ea841e77099b
AliImranGezgen <aliimrangzgn23@gmail.com>
2026-04-12 22:42:37 +0300

    Eventlerin gösterildiği sayfa tamamlandı.

--- frontend/src/app/router/index.tsx
+++ frontend/src/app/router/index.tsx
@@ -10,6 +10,7 @@ import LoginPage from '../../pages/public/LoginPage';
 import RegisterPage from '../../pages/public/RegisterPage';
 import UnauthorizedPage from '../../pages/public/UnauthorizedPage';
 import NotFoundPage from '../../pages/public/NotFoundPage';
+import EventsDiscoverPage from '../../pages/public/EventsDiscoverPage';
 
 import StudentDashboard from '../../pages/student/DashboardPage';
 import EventsPage from '../../pages/student/EventsPage';
@@ -18,7 +19,7 @@ import ClubAdminDashboard from '../../pages/clubAdmin/DashboardPage';
 import SystemAdminDashboard from '../../pages/systemAdmin/DashboardPage';
 
 export const router = createBrowserRouter([
-  { path: '/', element: <MainLayout />, children: [{ index: true, element: <HomePage /> }, { path: 'unauthorized', element: <UnauthorizedPage /> }, { path: '*', element: <NotFoundPage /> }] },
+  { path: '/', element: <MainLayout />, children: [{ index: true, element: <HomePage /> }, { path: 'events', element: <EventsDiscoverPage /> }, { path: 'unauthorized', element: <UnauthorizedPage /> }, { path: '*', element: <NotFoundPage /> }] },
   { path: '/', element: <AuthLayout />, children: [{ path: 'login', element: <LoginPage /> }, { path: 'register', element: <RegisterPage /> }] },
   { path: '/student', element: <StudentLayout />, children: [{ path: 'dashboard', element: <StudentDashboard /> }, { path: 'events', element: <EventsPage /> }] },
   { path: '/club-admin', element: <ClubAdminLayout />, children: [{ path: 'dashboard', element: <ClubAdminDashboard /> }] },
